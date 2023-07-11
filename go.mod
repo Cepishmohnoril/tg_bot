aa359cf83a8e8e78c6e42571f27f07f0f8fa12f5
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2023-07-11 16:05:22 +0300

    api module replaced, button interface created

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module tg_bot
 
 go 1.20
 
-require (
-	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.20
-	github.com/joho/godotenv v1.5.1
-)
+require github.com/joho/godotenv v1.5.1
+
+require gopkg.in/telebot.v3 v3.1.3
--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
 	"time"
 
-	"github.com/PaulSonOfLars/gotgbot/v2"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/message"
 	"github.com/joho/godotenv"
+	"gopkg.in/telebot.v3"
+)
+
+var (
+	// Universal markup builders.
+	menu = &telebot.ReplyMarkup{ResizeKeyboard: true}
+
+	// Reply buttons.
+	btnSendVid = menu.Text("Надіслати відео і опис")
 )
 
 func init() {
@@ -28,57 +30,35 @@ func main() {
 		panic("Telegram API token is absent.")
 	}
 
-	b, err := gotgbot.NewBot(token, &gotgbot.BotOpts{
-		Client: http.Client{},
-		DefaultRequestOpts: &gotgbot.RequestOpts{
-			Timeout: gotgbot.DefaultTimeout,
-			APIURL:  gotgbot.DefaultAPIURL,
-		},
-	})
-
-	if err != nil {
-		panic("failed to create new bot: " + err.Error())
+	pref := telebot.Settings{
+		Token:  token,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 	}
 
-	updater := ext.NewUpdater(&ext.UpdaterOpts{
-		Dispatcher: ext.NewDispatcher(&ext.DispatcherOpts{
-			Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
-				log.Println("an error occurred while handling update:", err.Error())
-				return ext.DispatcherActionNoop
-			},
-			MaxRoutines: ext.DefaultMaxRoutines,
-		}),
-	})
-	dispatcher := updater.Dispatcher
-	dispatcher.AddHandler(handlers.NewMessage(message.Text, forward))
-
-	// Start receiving updates.
-	err = updater.StartPolling(b, &ext.PollingOpts{
-		DropPendingUpdates: true,
-		GetUpdatesOpts: gotgbot.GetUpdatesOpts{
-			Timeout: 9,
-			RequestOpts: &gotgbot.RequestOpts{
-				Timeout: time.Second * 10,
-			},
-		},
-	})
+	b, err := telebot.NewBot(pref)
 	if err != nil {
-		panic("failed to start polling: " + err.Error())
+		log.Fatal(err)
+		return
 	}
-	log.Printf("%s has been started...\n", b.User.Username)
 
-	// Idle, to keep updates coming in, and avoid bot stopping.
-	updater.Idle()
+	initMenu()
+
+	b.Handle("/start", func(c telebot.Context) error {
+		return c.Send("Hello!", menu)
+	})
+
+	b.Handle(&btnSendVid, sendVid)
+
+	b.Start()
+
 }
 
-func forward(b *gotgbot.Bot, ctx *ext.Context) error {
-	chatIdStr, _ := os.LookupEnv("OUTPUT_CHAT_ID")
-	outChatId, _ := strconv.ParseInt(chatIdStr, 10, 64)
+func initMenu() {
+	menu.Reply(
+		menu.Row(btnSendVid),
+	)
+}
 
-	_, err := b.ForwardMessage(outChatId, ctx.EffectiveChat.Id, ctx.Message.MessageId, nil)
-
-	if err != nil {
-		return fmt.Errorf("failed to echo message: %w", err)
-	}
-	return nil
+func sendVid(c telebot.Context) error {
+	return c.Send("Відео надіслано")
 }
